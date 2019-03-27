dbf8d37faaf6e070752f457c326a2205597caf9d
Raul Mantilla <raulmantilla@macbook-air-de-raul.local>
2019-03-26 20:32:20 -0500

    Initial Commit

---
+++ SpotifyDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotifyDemo
+//
+//  Created by Raul Mantilla on 26/03/19.
+//  Copyright © 2019 Raul Mantilla Assia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
